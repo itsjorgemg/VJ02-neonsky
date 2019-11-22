519afb64065886542ce65da9bf2b494bb04d6404
David <david.garcia.de.mercado@est.fib.upc.edu>
2019-11-22 11:58:38 +0100

    Adds jumpingTile and its behavior.

---
+++ Assets/Scripts/JumpingTileBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingTileBehavior : MonoBehaviour
+{
+
+    [SerializeField] private float jumpForce = 6;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.gameObject.tag == "Player") collision.rigidbody.AddForce(0, jumpForce, 0, ForceMode.Impulse);
+    }
+}
