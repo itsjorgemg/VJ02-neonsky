7ea53b74acdd41724b8440fa464a43933fcdf7de
Jorge Mg <44137164+itsjorgemg@users.noreply.github.com>
2019-12-08 20:37:16 +0100

    Vanishing tile fixes

--- Assets/Scripts/VanishingTileBehavior.cs
+++ Assets/Scripts/VanishingTileBehavior.cs
@@ -5,50 +5,42 @@ using UnityEngine;
 public class VanishingTileBehavior : MonoBehaviour
 {
     private float fadeOutDuration = 0.5f;
-    private bool trigger = false;
 
     IEnumerator fadeOutCoroutine;
     private Color iniColor;
     private Color transparentColor;
-    private float timeIniVanish;
 
     // Start is called before the first frame update
     void Start()
     {
-        iniColor = new Color(1, 1, 1, 1);
-        transparentColor = new Color(1, 1, 1, 0);
+        iniColor = GetComponentInChildren<MeshRenderer>().material.color;
+        transparentColor = new Color(iniColor.r, iniColor.g, iniColor.b, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (fadeOutCoroutine == null && trigger) {
-            fadeOutCoroutine = FadeOut();
-            StartCoroutine(fadeOutCoroutine);
-        }
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            if (!trigger) {
-                gameObject.GetComponentsInChildren<AudioSource>()[0].Play();
-                timeIniVanish = Time.time;
-                trigger = true;
-                for (int i = 0; i < 5; ++i) gameObject.GetComponentsInChildren<MeshRenderer>()[i].material.color = iniColor;
-            }
+        if (other.CompareTag("Player") && fadeOutCoroutine == null) {
+            GetComponent<AudioSource>().Play();
+            fadeOutCoroutine = FadeOut();
+            StartCoroutine(fadeOutCoroutine);
         }
     }
 
     IEnumerator FadeOut()
     {
         for (var t = 0f; t < fadeOutDuration; t += Time.deltaTime) {
-            for (int i = 0; i < 5; ++i) {
-                gameObject.GetComponentsInChildren<MeshRenderer>()[i].material.color = Color.Lerp(iniColor, transparentColor, t / fadeOutDuration);
-                if (Time.time - timeIniVanish >= fadeOutDuration) gameObject.GetComponentsInChildren<BoxCollider>()[i].enabled = false;
-            }
+            foreach (var renderer in gameObject.GetComponentsInChildren<MeshRenderer>())
+                renderer.material.color = Color.Lerp(iniColor, transparentColor, t / fadeOutDuration);
+            
             yield return null;
         }
+        foreach (var collider in gameObject.GetComponentsInChildren<BoxCollider>())
+            collider.enabled = false;
     }
 }
