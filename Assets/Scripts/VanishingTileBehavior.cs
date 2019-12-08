44c853fcb354555f0cb36d6c1446bc6ed51de4e2
David <david.garcia.de.mercado@est.fib.upc.edu>
2019-12-08 13:21:22 +0100

    Adds Vanishing tile column. Pending sounds

---
+++ Assets/Scripts/VanishingTileBehavior.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VanishingTileBehavior : MonoBehaviour
+{
+    private float fadeOutDuration = 0.75f;
+    private bool trigger = false;
+
+    IEnumerator fadeOutCoroutine;
+    private Color iniColor;
+    private Color transparentColor;
+    private float timeIniVanish;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        iniColor = new Color(1, 1, 1, 1);
+        transparentColor = new Color(1, 1, 1, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fadeOutCoroutine == null && trigger) {
+            fadeOutCoroutine = FadeOut();
+            StartCoroutine(fadeOutCoroutine);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (!trigger) {
+                timeIniVanish = Time.time;
+                trigger = true;
+                //gameObject.GetComponentsInChildren<AudioSource>()[0].Play();
+                for (int i = 0; i < 5; ++i) gameObject.GetComponentsInChildren<MeshRenderer>()[i].material.color = iniColor;
+            }
+        }
+    }
+
+    IEnumerator FadeOut()
+    {
+        //gameObject.GetComponentsInChildren<AudioSource>()[0].Play();
+        for (var t = 0f; t < fadeOutDuration; t += Time.deltaTime) {
+            for (int i = 0; i < 5; ++i) {
+                gameObject.GetComponentsInChildren<MeshRenderer>()[i].material.color = Color.Lerp(iniColor, transparentColor, t / fadeOutDuration);
+                if (Time.time - timeIniVanish >= 0.75) gameObject.GetComponentsInChildren<BoxCollider>()[i+1].enabled = false;
+            }
+            yield return null;
+        }
+    }
+}
