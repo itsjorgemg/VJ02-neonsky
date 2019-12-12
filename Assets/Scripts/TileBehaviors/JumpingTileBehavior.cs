d28b13c63412c7a67f36eccb8e5a4f8bd7bd209f
Jorge Mg <44137164+itsjorgemg@users.noreply.github.com>
2019-12-12 22:11:01 +0100

    Jumping tile animation effect

--- Assets/Scripts/TileBehaviors/JumpingTileBehavior.cs
+++ Assets/Scripts/TileBehaviors/JumpingTileBehavior.cs
@@ -5,23 +5,40 @@ using UnityEngine;
 public class JumpingTileBehavior : MonoBehaviour
 {
 
-    [SerializeField] private float jumpForce = 3;
+    [SerializeField] private float jumpForce = 7.65f;
+    [SerializeField] private float animationDuration = 0.5f;
+    [SerializeField] private float animationScaleY = 1.5f;
+    private bool animate = false;
+    private float startTime;
+    private Vector3 iniScale;
+
     // Start is called before the first frame update
     void Start()
     {
+        iniScale = transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (animate) {
+            // (sin(x - π/2 + (1 - 0.1)) + 1) * 0.5 * 1.5
+            float sinus = (Mathf.Sin(((Time.time - startTime) * (360.0f / animationDuration) - 90) * Mathf.Deg2Rad + (1 - iniScale.y)) + 1) * 0.5f * animationScaleY;
+            transform.localScale = new Vector3(iniScale.x, sinus, iniScale.z);
+            if (sinus <= iniScale.y) {
+                animate = false;
+                transform.localScale = iniScale;
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player")) {
-            gameObject.GetComponentsInChildren<AudioSource>()[0].Play();
+        if (other.CompareTag("Player") && !animate) {
+            GetComponent<AudioSource>().Play();
             other.attachedRigidbody.AddForce(0, jumpForce, 0, ForceMode.Impulse);
+            animate = true;
+            startTime = Time.time;
         }
     }
 }
